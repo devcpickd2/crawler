9caf225b54c656dc0588b9debe518ef5129d2a72
Efa-Isnawati <efaisnawati1911@gmail.com>
2024-04-17 16:11:10 +0700

    refactor : intialize controllers adn styling the interface

--- application/controllers/Pegawai.php
+++ application/controllers/Pegawai.php
@@ -1,17 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home extends CI_Controller {
-
-	// public function __construct()
-    // {
-    //     parent::__construct();
-    // }
+class Pegawai extends CI_Controller {
 
 	public function index()
 	{
 		$this->load->view('partials/head');
-		$this->load->view('home/home');
+		$this->load->view('pegawai/pegawai');
 		$this->load->view('partials/footer');
 	}
 }
---
+++ application/views/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white" style="padding-bottom: 20px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <span>Copyright &copy; CPI Cikande 2 Retort 2024</span>
+            </div>
+        </div>
+    </div>
+</footer>
